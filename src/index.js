5974ab6074800f276bc049fb4bb3e764a57b0ce9
PavelZakharov <pavelzakharov1987@gmail.com>
2020-03-22 22:12:41 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arrayStr = str.split( '' );
+
+    for (let i = 0; i < arrayStr.length; i++) {
+		for (let j = 0; j < bracketsConfig.length; j++) {
+			if (arrayStr[i] === bracketsConfig[j][0] && arrayStr[i + 1] === bracketsConfig[j][1]) {
+                arrayStr.splice(i, 2);
+                i = -1;
+            }
+		}
+		if (arrayStr.length === 0){
+            return true;
+        }
+	}
+
+	return false;
 }
